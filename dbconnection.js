00649fb1b35c334036efad90c21281612255ba65
Lucas Martins <lucas.pemartins@gmail.com>
2017-07-08 16:08:22 -0300

    Edit dbconection to test jawsdb connection

--- dbconnection.js
+++ dbconnection.js
@@ -1,10 +1,10 @@
 var mysql=require('mysql');
 var connection=mysql.createPool({
  
- host:'localhost',
- user:'root',
- password:'root',
- database:'SELL_ON_CREDIT_DB'
+ host:'bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+ user:'wuv0yl1t364wrojl',
+ password:'igs6h4p4z2t44xsv',
+ database:'mrr3bj8ub8gd6imi'
  
 });
 module.exports=connection;
