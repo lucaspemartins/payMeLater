927968fc577a316fc46991cbb95c5cb2abe91d47
Lucas Martins <lucas.pemartins@gmail.com>
2017-09-06 22:12:43 -0300

    Modify app.js to test

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'routes')));
 
 app.use('/', index);
 //app.use('/users', users);
