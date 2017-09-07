646868e430f8caf0905eedeba3da99222009549b
Lucas Martins <lucas.pemartins@gmail.com>
2017-09-06 21:59:43 -0300

    Add node_modules to static folders

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', index);
 //app.use('/users', users);
