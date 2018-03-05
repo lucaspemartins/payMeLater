6bfbe2c6c69e82940339273bf26161444c3f2982
Lucas Martins <lucas.pemartins@gmail.com>
2018-03-04 23:03:28 -0300

    Fix table names

--- models/Customers.js
+++ models/Customers.js
@@ -3,13 +3,13 @@ var db = require('../dbconnection'); //reference of dbconnection.js
 var Customers = {
 
     getAllCustomers: function(vendor_cpf, callback) {
-        return db.query("select * from Users where cpf in (select customer_cpf from Vendors_Has_Customers where vendor_cpf=?)", [vendor_cpf], callback);
+        return db.query("select * from users where cpf in (select customer_cpf from vendors_has_customers where vendor_cpf=?)", [vendor_cpf], callback);
     },
     addCustomer: function(vendor_cpf, customer_cpf, callback) {
-        return db.query("insert into Vendors_Has_Customers(vendor_cpf, customer_cpf) values(?,?)", [vendor_cpf, customer_cpf], callback);
+        return db.query("insert into vendors_has_customers(vendor_cpf, customer_cpf) values(?,?)", [vendor_cpf, customer_cpf], callback);
     },
     deleteCustomer: function(vendor_cpf, customer_cpf, callback) {
-        return db.query("delete from Vendors_Has_Customers where vendor_cpf=? and customer_cpf=?", [vendor_cpf, customer_cpf], callback);
+        return db.query("delete from vendors_has_customers where vendor_cpf=? and customer_cpf=?", [vendor_cpf, customer_cpf], callback);
     }
 
 };
--- models/Users.js
+++ models/Users.js
@@ -3,28 +3,28 @@ var db = require('../dbconnection'); //reference of dbconnection.js
 var Users = {
 
     getAllUsers: function(callback) {
-        return db.query("select * from Users", callback);
+        return db.query("select * from users", callback);
     },
     getAllUsersWithoutMe: function(cpf, callback) {
-        return db.query("select * from Users where cpf not like ?", [cpf], callback);
+        return db.query("select * from users where cpf not like ?", [cpf], callback);
     },
     getUserByCpf: function(cpf, callback) {
-        return db.query("select * from Users where cpf=?", [cpf], callback);
+        return db.query("select * from users where cpf=?", [cpf], callback);
     },
     getCpfByNickname: function(nickname, callback) {
-        return db.query("select cpf from Users where nickname=?", [nickname], callback);
+        return db.query("select cpf from users where nickname=?", [nickname], callback);
     },
     getUserByEMailAndPass: function(email, password, callback) {
-        return db.query("select * from Users where email=? and password=?", [email, password], callback);
+        return db.query("select * from users where email=? and password=?", [email, password], callback);
     },
     addUser: function(Users, callback) {
-        return db.query("insert into Users(cpf, nickname, user_name, password, cellphone, telephone, email) values(?,?,?,?,?,?)", [Users.cpf, Users.nickname, Users.user_name, Users.password, Users.cellphone, Users.telephone, Users.email], callback);
+        return db.query("insert into users(cpf, nickname, user_name, password, cellphone, telephone, email) values(?,?,?,?,?,?)", [Users.cpf, Users.nickname, Users.user_name, Users.password, Users.cellphone, Users.telephone, Users.email], callback);
     },
     deleteUser: function(cpf, callback) {
-        return db.query("delete from Users where cpf=?", [cpf], callback);
+        return db.query("delete from users where cpf=?", [cpf], callback);
     },
     updateUser: function(cpf, Users, callback) {
-        return db.query("update Users set user_name=?,nickname=?,cellphone=?,telephone=?,email=? where cpf=?", [Users.user_name, Users.nickname, Users.cellphone, Users.telephone, Users.email, cpf], callback);
+        return db.query("update users set user_name=?,nickname=?,cellphone=?,telephone=?,email=? where cpf=?", [Users.user_name, Users.nickname, Users.cellphone, Users.telephone, Users.email, cpf], callback);
     }
 
 };
