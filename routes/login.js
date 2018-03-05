1bf22e4a1515b2ed1faa300dc38930b9550637ba
Lucas Martins <lucas.pemartins@gmail.com>
2018-03-05 00:49:23 -0300

    Change return type on login service

--- routes/login.js
+++ routes/login.js
@@ -4,11 +4,11 @@ var Users = require('../models/Users');
 
 router.get('/:email?/:password?', function (req, res, next) {
     if (req.params.email && req.params.password) {
-        Users.getUserByEMailAndPass(req.params.email, req.params.password, function (err, rows) {
+        Users.getUserByEMailAndPass(req.params.email, req.params.password, function (err, count) {
             if (err) {
                 res.json(err);
             } else {
-                res.json(rows);
+                res.json(count);
             }
         });
     }
