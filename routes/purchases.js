37cb8b50d7431d0de3f7a01b3951844f83485524
Lucas Martins <lucas.pemartins@gmail.com>
2018-05-01 22:57:28 -0300

    Fix purhcases route

--- routes/purchases.js
+++ routes/purchases.js
@@ -4,13 +4,13 @@ var Purchases = require('../models/Purchases');
 
 router.get('/:customer_cpf?', function (req, res, next) {
 
-        Sales.getAllPurchasesByCustomer(req.params.customer_cpf, function (err, rows) {
-            if (err) {
-                res.json(err);
-            } else {
-                res.json(rows);
-            }
-        });
+    Purchases.getAllPurchasesByCustomer(req.params.customer_cpf, function (err, rows) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(rows);
+        }
+    });
 });
 
 module.exports = router;
